6a6ac03ed227a0308a0635b4bc650ce75e0bba0c
TiagoSpada <tiago.spada.ts@gmail.com>
2024-07-27 17:24:15 +0200

    [upd] creazione model, migration, seeder e controller

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -5,6 +5,17 @@
         <h1>{{ $project->title }}</h1>
         <p class="fs-5 mt-4">{{ $project->description }}</p>
         <hr>
-        <a href="{{ route('admin.projects.edit', $project) }}" class="btn btn-warning fs-5">Modifica</a>
+        <div class="d-flex gap-2 align-items-center">
+            <a href="{{ route('admin.projects.edit', $project) }}" class="btn btn-warning fs-6" as="button">
+                Modifica
+            </a>
+            <form action="{{ route('admin.projects.destroy', $project) }}" method="POST">
+                @csrf
+                @method('DELETE')
+
+                <button class="btn btn-danger fs-6">Elimina</button>
+
+            </form>
+        </div>
     </div>
 @endsection
